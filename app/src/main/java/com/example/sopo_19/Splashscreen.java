b1b91edf4d93be9d0dae4edcec73bd88d04a3e08
Agus Michael Sianipar <43785502+agusmichaelsianipar@users.noreply.github.com>
2020-04-30 12:52:02 +0700

    Splashscreen

---
+++ app/src/main/java/com/example/sopo_19/Splashscreen.java
@@ -0,0 +1,26 @@
+package com.example.sopo_19;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.Intent;
+import android.os.Handler;
+import android.os.Bundle;
+import android.view.Window;
+
+public class Splashscreen extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_splashscreen);
+
+        final Handler handler = new Handler();
+        handler.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Splashscreen.this, MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        },3000);
+    }
+}
